33126dfc2e4837719ed78ed07d429cd35b025f95
oyeonjoo <oyj0842@naver.com>
2024-03-22 02:57:41 +0900

    상품 entity, repository 설계 및 테스트

---
+++ src/main/java/com/shop/entity/Item.java
@@ -0,0 +1,50 @@
+package com.shop.entity;
+
+import com.shop.constant.ItemSellStatus;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "item")
+@ToString
+public class Item {
+
+    @Id
+    @Column(name = "item_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String itemNm;
+
+    @Column(name = "price", nullable = false)
+    private int price;
+
+    @Column(nullable = false)
+    private int stockNumber;
+
+    @Lob
+    @Column(nullable = false)
+    private String itemDetail;
+
+    @Enumerated(EnumType.STRING)
+    private ItemSellStatus itemSellStatus;
+
+    private LocalDateTime regTime;
+
+    private LocalDateTime updateTime;
+
+}
