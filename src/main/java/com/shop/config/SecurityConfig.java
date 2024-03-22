4300b0c6bf6a227dd371e531037b28c9dc357d83
oyeonjoo <oyj0842@naver.com>
2024-03-23 01:11:08 +0900

    로그인/로그아웃 기능 구현 및 테스트

--- src/main/java/com/shop/config/SecurityConfig.java
+++ src/main/java/com/shop/config/SecurityConfig.java
@@ -8,20 +8,35 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig
+//    extends
+//    SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity>
+{
+
+//    @Autowired
+//    MemberService memberService;
+//    private AuthenticationEntryPoint customAuthenticationEntryPoint;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http.formLogin((formLogin) ->
-//            formLogin.loginPage("/members/login")
-//                .defaultSuccessUrl("/")
-//                .usernameParameter("email")
-//                .failureUrl("/members/login/error")
-//        );
-//        ;
+//        http.csrf(AbstractHttpConfigurer::disable);
+
+        http.formLogin((formLogin) ->
+                formLogin
+                    .loginPage("/members/login")
+                    .defaultSuccessUrl("/")
+                    .usernameParameter("email")
+                    .failureUrl("/members/login/error")
+            )
+            .logout(logout ->
+                logout
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout"))
+                    .logoutSuccessUrl("/")
+            );
 
         http.authorizeHttpRequests(authorizeHttpRequests ->
             authorizeHttpRequests
@@ -39,4 +54,16 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+//
+//        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.userDetailsService(memberService)
+//            .passwordEncoder(passwordEncoder());
+//    }
+//    @Override
+//    public void configure(HttpSecurity http) throws Exception {
+//        http
+//            .exceptionHandling(exceptionHandling -> exceptionHandling
+//            .authenticationEntryPoint(customAuthenticationEntryPoint));
+//    }
 }
--- src/main/java/com/shop/entity/Member.java
+++ src/main/java/com/shop/entity/Member.java
@@ -39,15 +39,16 @@ public class Member {
     @Enumerated(EnumType.STRING)
     private Role role;
 
-    public static Member createMember(MemberFormDto memberFormDto, PasswordEncoder passwordEncoder) {
+    public static Member createMember(MemberFormDto memberFormDto,
+        PasswordEncoder passwordEncoder) {
 
         Member member = new Member();
         member.setName(memberFormDto.getName());
         member.setEmail(memberFormDto.getEmail());
         member.setAddress(member.getAddress());
-        String  password = passwordEncoder.encode(memberFormDto.getPassword());
+        String password = passwordEncoder.encode(memberFormDto.getPassword());
         member.setPassword(password);
-        member.setRole(Role.USER);
+        member.setRole(Role.ADMIN);
 
         return member;
     }
