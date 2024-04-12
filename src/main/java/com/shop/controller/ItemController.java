a7211148f00dc0069f4278e29b2dc236e84c232f
oyeonjoo <oyj0842@naver.com>
2024-04-12 15:36:28 +0900

    :sparkles: 등록한 상품 조회 기능구현

--- src/main/java/com/shop/controller/ItemController.java
+++ src/main/java/com/shop/controller/ItemController.java
@@ -2,6 +2,7 @@ package com.shop.controller;
 
 import com.shop.dto.ItemFormDto;
 import com.shop.service.ItemService;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -26,7 +28,8 @@ public class ItemController {
     }
 
     @PostMapping(value = "/admin/item/new")
-    public String itemNew(@Valid ItemFormDto itemFormDto,
+    public String itemNew(
+        @Valid ItemFormDto itemFormDto,
         BindingResult bindingResult,
         Model model,
         @RequestParam("itemImgFile") List<MultipartFile> itemImgFileList) {
@@ -48,5 +51,23 @@ public class ItemController {
         }
 
         return "redirect:/";
+    }
+
+    @GetMapping(value = "/admin/item/{itemId}")
+    public String itemDtl(
+        @PathVariable Long itemId,
+        Model model
+    ) {
+        try {
+            ItemFormDto itemFormDto = itemService.getItemDtl(itemId);
+            model.addAttribute("itemFormDto", itemFormDto);
+        } catch (EntityNotFoundException e) {
+            model.addAttribute("errorMessage", "존재하지 않는 상품 입니다.");
+            model.addAttribute("itemDto", new ItemFormDto());
+
+            return "item/itemForm";
+        }
+
+        return "item/itemForm";
     }
 }
--- src/main/java/com/shop/service/ItemService.java
+++ src/main/java/com/shop/service/ItemService.java
@@ -1,10 +1,13 @@
 package com.shop.service;
 
 import com.shop.dto.ItemFormDto;
+import com.shop.dto.ItemImgDto;
 import com.shop.entity.Item;
 import com.shop.entity.ItemImg;
 import com.shop.repository.ItemImgRepository;
 import com.shop.repository.ItemRepository;
+import jakarta.persistence.EntityNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -28,15 +31,15 @@ public class ItemService {
         itemRepository.save(item);
 
         //이미지 등록
-        for(int i = 0; i < itemImgFileList.size(); i++){
+        for (int i = 0; i < itemImgFileList.size(); i++) {
             ItemImg itemImg = new ItemImg();
             itemImg.setItem(item);
 
-            if(i == 0)  {
+            if (i == 0) {
                 itemImg.setRepImgYn("Y");
-            }
-            else
+            } else {
                 itemImg.setRepImgYn("N");
+            }
 
             itemImgService.saveItemImg(itemImg, itemImgFileList.get(i));
         }
@@ -44,5 +47,22 @@ public class ItemService {
         return item.getId();
     }
 
+    @Transactional(readOnly = true)
+    public ItemFormDto getItemDtl(Long itemId) {
+
+        List<ItemImg> itemImgList = itemImgRepository.findByItemIdOrderByIdAsc(itemId);
+
+        List<ItemImgDto> itemImgDtoList = new ArrayList<>();
+        for (ItemImg itemImg : itemImgList) {
+            ItemImgDto itemImgDto = ItemImgDto.of(itemImg);
+            itemImgDtoList.add(itemImgDto);
+        }
+
+        Item item = itemRepository.findById(itemId)
+            .orElseThrow(EntityNotFoundException::new);
+        ItemFormDto itemFormDto = ItemFormDto.of(item);
+        itemFormDto.setItemImgDtoList(itemImgDtoList);
+        return itemFormDto;
+    }
 
 }
