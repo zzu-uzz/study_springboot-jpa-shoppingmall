863e9006d022d675b1cd53cc2a632b0441e3c4d0
oyeonjoo <oyj0842@naver.com>
2024-04-13 05:39:11 +0900

    :recycle: 불필요한 주석 제거

--- src/main/java/com/shop/controller/MemberController.java
+++ src/main/java/com/shop/controller/MemberController.java
@@ -28,15 +28,6 @@ public class MemberController {
         return "member/memberForm";
     }
 
-//    @PostMapping(value = "/new")
-//    public String memberForm(MemberFormDto memberFormDto) {
-//
-//        Member member = Member.createMember(memberFormDto, passwordEncoder);
-//        memberService.saveMember(member);
-//
-//        return "redirect:/";
-//    }
-
     @PostMapping(value = "/new")
     public String newMember(@Valid MemberFormDto memberFormDto,
         BindingResult bindingResult, Model model) {
