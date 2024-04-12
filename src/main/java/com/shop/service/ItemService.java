6590e2c9a414b6b0a9c7fe1dca5f95eea94511ad
oyeonjoo <oyj0842@naver.com>
2024-04-12 21:58:25 +0900

    :sparkles: @QueryProjection 을 사용한 메인 화면 구현

--- src/main/java/com/shop/service/ItemService.java
+++ src/main/java/com/shop/service/ItemService.java
@@ -3,6 +3,7 @@ package com.shop.service;
 import com.shop.dto.ItemFormDto;
 import com.shop.dto.ItemImgDto;
 import com.shop.dto.ItemSearchDto;
+import com.shop.dto.MainItemDto;
 import com.shop.entity.Item;
 import com.shop.entity.ItemImg;
 import com.shop.repository.ItemImgRepository;
@@ -96,4 +97,11 @@ public class ItemService {
         return itemRepository.getAdminItemPage(itemSearchDto, pageable);
     }
 
+    @Transactional(readOnly = true)
+    public Page<MainItemDto> getMainItemPage(
+        ItemSearchDto itemSearchDto,
+        Pageable pageable
+    ) {
+        return itemRepository.getMainItemPage(itemSearchDto, pageable);
+    }
 }
