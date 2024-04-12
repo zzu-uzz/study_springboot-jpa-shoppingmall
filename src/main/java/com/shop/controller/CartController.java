973ae6034fbb69f1e2a0411fe15a7bf3546a271f
oyeonjoo <oyj0842@naver.com>
2024-04-13 03:43:09 +0900

    :sparkles: 장바구니 담기 기능구현

---
+++ src/main/java/com/shop/controller/CartController.java
@@ -0,0 +1,53 @@
+package com.shop.controller;
+
+import com.shop.dto.CartItemDto;
+import com.shop.service.CartService;
+import jakarta.validation.Valid;
+import java.security.Principal;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequiredArgsConstructor
+public class CartController {
+
+    private final CartService cartService;
+
+    @PostMapping(value = "/cart")
+    public @ResponseBody ResponseEntity order(
+        @RequestBody @Valid CartItemDto cartItemDto,
+        BindingResult bindingResult,
+        Principal principal
+    ) {
+        if (bindingResult.hasErrors()) {
+            StringBuilder sb = new StringBuilder();
+            List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+
+            for (FieldError fieldError : fieldErrors) {
+                sb.append(fieldError.getDefaultMessage());
+            }
+            return new ResponseEntity<String>(sb.toString(),
+                HttpStatus.BAD_REQUEST);
+        }
+
+        String email = principal.getName();
+        Long cartItemId;
+
+        try {
+            cartItemId = cartService.addCart(cartItemDto, email);
+        } catch (Exception e) {
+            return new ResponseEntity<String>(e.getMessage(),
+                HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<Long>(cartItemId,
+            HttpStatus.OK);
+    }
+}
--- src/main/java/com/shop/entity/CartItem.java
+++ src/main/java/com/shop/entity/CartItem.java
@@ -31,4 +31,17 @@ public class CartItem extends BaseEntity {
     private Item item;
 
     private int count;
+
+    public static CartItem creteCartItem(Cart cart, Item item, int count) {
+        CartItem cartItem = new CartItem();
+        cartItem.setCart(cart);
+        cartItem.setItem(item);
+        cartItem.setCount(count);
+
+        return cartItem;
+    }
+
+    public void addCount(int count){
+        this.count += count;
+    }
 }
